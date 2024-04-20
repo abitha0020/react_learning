214d16b93f30f8affe89ed9e35ff740d0e8a9823
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-04-20 12:26:51 +0530

    updates

--- my-first-react-app/src/greetings.jsx
+++ my-first-react-app/src/greetings.jsx
@@ -1,17 +1,14 @@
 function Greetings() {
-    return(
+  const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  const animalsList = animals.map((animal) => <li key={animal}>{animal}</li>)
+
+  return (
     <div>
-  <h1>Test title</h1>
-  <ol className="test-list">
-    <li>List item 1</li>   
-    <li>List item 2</li>
-    <li>List item 3</li>
-  </ol>
-  <svg>
-     <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
-  </svg>
-  <form><input type="text"/></form>
-</div>
-    )
+      <h1>Animals: </h1>
+      <ul>
+        {animalsList}
+      </ul>
+    </div>
+  );
 }
 export default Greetings;
